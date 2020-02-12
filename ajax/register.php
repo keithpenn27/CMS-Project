d7c81bbc3094cb6fbde0ad270b2d86cd1ced318f
Keith Pennington <keith.penn27@gmail.com>
2020-02-11 22:29:37 -0600

    Added registration via ajax

--- ajax/register.php
+++ ajax/register.php
@@ -12,20 +12,46 @@
         
         $response = [];
 
+        $email = Filter::String($_POST['email']);
+
         // Make sure the user does not exist
+        $findUser = $con->prepare("SELECT user_id FROM users WHERE email = LOWER(:email) LIMIT 1");
+        $findUser->bindParam(':email', $email, PDO::PARAM_STR);
+        $findUser->execute();
+
+        if ($findUser->rowCount() == 1) {
+            // User exists
+            $response['error'] = "An account has already been registered with that email address. Please log in.";
+            $response['is_logged_in'] = false;
+        } else {
+            // User does not exist, add them now.
+
+            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+            $addUser = $con->prepare("INSERT INTO users(email, password) VALUES(LOWER(:email), :password)");
+            $addUser->bindParam(':email', $email, PDO::PARAM_STR);
+            $addUser->bindParam(':password', $password, PDO::PARAM_STR);
+            $addUser->execute();
+
+            $user_id = $con->lastInsertId();
+
+            $_SESSION['user_id'] = (int) $user_id;
+
+            $response['redirect'] = 'dashboard.php?message=welcome';
+            $response['is_logged_in'] = true;
+        }
 
         // Make sure the user CAN be added AND is added
 
         // Return the proper information back to JavaScript to redirect us.
 
         $response['redirect'] = 'dashboard.php';
-        $response['name'] = 'Keith Pennington';
 
         echo json_encode($response, JSON_PRETTY_PRINT);
         exit;
 
     } else {
         // Die. Kill the scripe. Redirect the user.
-        exit('test');
+        exit('Invalid URL');
     }
 ?>
--- inc/config.php
+++ inc/config.php
@@ -7,9 +7,9 @@
 
     // Our config is below
 
-    
-	// Include the DB.php file;
+	// Include the class files
 	include_once "classes/DB.php";
+	include_once "classes/Filter.php";
 
     $con = DB::getConnection();
     
